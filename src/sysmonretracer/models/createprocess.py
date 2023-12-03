049d947b84d8eb8ad1a87ef167e7ad6aa774d39b
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2023-12-03 17:34:27 +0000

    feat: rewrite with pandas

--- src/sysmonretracer/models/createprocess.py
+++ src/sysmonretracer/models/createprocess.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from dataclasses import dataclass
 from sysmonretracer.models.base import BaseEvent
 from sysmonretracer.utils import parse_str_to_datetime
@@ -7,11 +8,11 @@ from sysmonretracer.utils import parse_str_to_datetime
 class CreateProcessEvent(BaseEvent):
 
     @classmethod
-    def from_dict(cls, d: dict):
+    def from_dict(cls, df: pd.DataFrame):
         return cls(
-            ProcessGuid=d['Event']['EventData']['ProcessGuid'],
-            ProcessId=d['Event']['EventData']['ProcessId'],
-            ParentProcessGuid=d['Event']['EventData']['ParentProcessGuid'],
-            EventId=d['Event']['System']['EventID'],
-            Timestamp=parse_str_to_datetime(d['Event']['EventData']['UtcTime']),
+            ProcessGuid=df['Event.EventData.ProcessGuid'],
+            ProcessId=df['Event.EventData.ProcessId'],
+            ParentProcessGuid=df['Event.EventData.ParentProcessGuid'],
+            EventId=df['Event.System.EventID'],
+            Timestamp=parse_str_to_datetime(df['Event.EventData.UtcTime']),
         )
--- src/sysmonretracer/run.py
+++ src/sysmonretracer/run.py
@@ -1,7 +1,9 @@
+import io
 from pprint import pprint
 from sysmonretracer.models.createprocess import CreateProcessEvent
 
 import orjson
+import pandas as pd
 from evtx import PyEvtxParser
 
 def main():
@@ -10,17 +12,15 @@ def main():
     }
 
     parser = PyEvtxParser("./LM_typical_IIS_webshell_sysmon_1_10_traces.evtx")
-    for record in parser.records_json():
-        d = orjson.loads(record['data'])
+    df = pd.concat([pd.json_normalize(orjson.loads(r['data'])) for r in parser.records_json()])
 
-        event_model = models[d['Event']['System']['EventID']]
-        event = event_model.from_dict(d)
-        print(event)
-
-        # try:
-        # except:
-        #     pass
-        #     pprint(d)
+    for _, df in df.iterrows():
+        try:
+            event_model = models[df['Event.System.EventID']]
+            event = event_model.from_dict(df)
+            print(event)
+        except KeyError:
+            pass
 
 if __name__ == '__main__':
     main()
