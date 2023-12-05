b564d0eda038520259f4b12677be4cd61e2944cf
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2023-12-05 16:32:51 +0000

    feat: cli

--- src/sysmonretracer/run.py
+++ src/sysmonretracer/run.py
@@ -1,9 +1,22 @@
-import sys
 from sysmonretracer.controller.EvtxController import EvtxController
 from sysmonretracer.controller.ModelController import ModelController
 
-def main():
-    df = EvtxController.load_to_dataframe(sys.argv[1])
+import click
+
+__version__ = '0.1.0'
+
+@click.command()
+@click.argument('eventlog')
+# @click.option('-sd', '--start-date', help='')
+# @click.option('-ed', '--end-date', help='')
+@click.help_option('-h', '--help')
+@click.version_option(__version__, '-v', '--version', message='%(prog)s v%(version)s')
+def main(
+    eventlog: str,
+    start_date: str,
+    end_date: str,
+):
+    df = EvtxController.load_to_dataframe(eventlog)
     gen_models = ModelController.generate_models(df)
 
     for m in gen_models:
