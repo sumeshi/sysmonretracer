c326c609314ba9f68063b52142dd1121b6507c28
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2023-12-05 17:10:00 +0000

    feat: find

--- src/sysmonretracer/run.py
+++ src/sysmonretracer/run.py
@@ -5,13 +5,38 @@ import click
 
 __version__ = '0.1.0'
 
-@click.command()
-@click.argument('eventlog')
-# @click.option('-sd', '--start-date', help='')
-# @click.option('-ed', '--end-date', help='')
+@click.group()
 @click.help_option('-h', '--help')
 @click.version_option(__version__, '-v', '--version', message='%(prog)s v%(version)s')
-def main(
+def cmd():
+    pass
+
+
+@cmd.command()
+@click.argument('query')
+@click.argument('eventlog')
+@click.option('-sd', '--start-date', help='')
+@click.option('-ed', '--end-date', help='')
+@click.option('-F', '--fixed-strings', is_flag=False, help='')
+@click.help_option('-h', '--help')
+def find(
+    eventlog: str,
+    query: str,
+    start_date: str,
+    end_date: str,
+    fixed_strings: bool,
+):
+    df = EvtxController.load_to_dataframe(eventlog)
+    df['Event.EventData.Image'].str.contains(query, regex=fixed_strings)
+    print(df)
+
+
+@cmd.command()
+@click.argument('eventlog')
+@click.option('-sd', '--start-date', help='')
+@click.option('-ed', '--end-date', help='')
+@click.help_option('-h', '--help')
+def retrace(
     eventlog: str,
     start_date: str,
     end_date: str,
@@ -23,4 +48,4 @@ def main(
         print(m)
 
 if __name__ == '__main__':
-    main()
+    cmd(obj={})
